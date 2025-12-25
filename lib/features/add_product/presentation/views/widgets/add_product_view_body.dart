0a3b68cb0c0fc603a5cc53c8ab8df5e3b2b8b836
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-25 11:27:29 +0100

    add product view body: add form

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class AddProductViewBody extends StatelessWidget {
+class AddProductViewBody extends StatefulWidget {
   const AddProductViewBody({super.key});
 
   @override
+  State<AddProductViewBody> createState() => _AddProductViewBodyState();
+}
+
+class _AddProductViewBodyState extends State<AddProductViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        autovalidateMode: autoValidateMode,
+        child: Column(children: []),
+      ),
+    );
   }
 }
