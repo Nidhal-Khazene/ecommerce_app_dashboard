52f2497b8ddde6485e35c185ed8ddc894fad2ff7
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-26 20:42:45 +0100

    add product view: add build App Bar

--- lib/features/add_product/presentation/views/add_product_view.dart
+++ lib/features/add_product/presentation/views/add_product_view.dart
@@ -2,6 +2,8 @@ import 'package:ecommerce_app_dashboard/constants.dart';
 import 'package:ecommerce_app_dashboard/features/add_product/presentation/views/widgets/add_product_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/helper/build_app_bar.dart';
+
 class AddProductView extends StatelessWidget {
   const AddProductView({super.key});
 
@@ -9,7 +11,8 @@ class AddProductView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: buildAppBar(title: "Add Product"),
       body: SafeArea(
         child: Padding(
           padding: kPrimaryScreenPadding,
