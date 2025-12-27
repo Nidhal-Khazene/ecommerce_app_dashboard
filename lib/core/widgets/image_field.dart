fa03f4cc5cd50495a3dcde4ee4eaff5ec5c3b600
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-27 12:16:49 +0100

    image field done

--- lib/core/widgets/image_field.dart
+++ lib/core/widgets/image_field.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:ecommerce_app_dashboard/core/utils/app_styles.dart';
 import 'package:ecommerce_app_dashboard/core/utils/colors_data.dart';
 import 'package:flutter/material.dart';
@@ -12,54 +14,110 @@ class ImageField extends StatefulWidget {
 }
 
 class _ImageFieldState extends State<ImageField> {
-  late bool isLoading = true;
+  bool isLoading = false;
+  File? fileImage;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: const Color(0xffF9FAFA),
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(width: 1, color: ColorsData.kBorderColor),
-      ),
-      child: GestureDetector(
-        onTap: () async {
-          setState(() {
-            isLoading = true;
-          });
-          final ImagePicker picker = ImagePicker();
-          final XFile? image = await picker.pickImage(
-            source: ImageSource.gallery,
-          );
-          setState(() {
-            isLoading = false;
-          });
-        },
-        child: Center(
-          child: Column(
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: ColorsData.kContainerFieldColor,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(width: 1, color: ColorsData.kBorderColor),
+          ),
+          child: Stack(
             children: [
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: Skeletonizer(
-                  enabled: isLoading,
-                  child: Icon(
-                    Icons.image_rounded,
-                    size: 180,
-                    color: const Color(0xFFC9CECE),
+              GestureDetector(
+                onTap: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    await pickImage();
+                  } catch (e) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                  }
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+                child: Center(
+                  child: Column(
+                    children: [
+                      fileImage != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.file(fileImage!),
+                            )
+                          : Padding(
+                              padding: EdgeInsets.symmetric(vertical: 6),
+                              child: Skeletonizer(
+                                enabled: isLoading,
+                                child: Icon(
+                                  Icons.image_rounded,
+                                  size: 180,
+                                  color: const Color(0xFFC9CECE),
+                                ),
+                              ),
+                            ),
+                    ],
                   ),
                 ),
               ),
-              Text(
-                "Product Image",
-                style: AppStyles.regular16.copyWith(
-                  color: const Color(0xFF949D9E),
+              Visibility(
+                visible: fileImage != null,
+                child: Positioned(
+                  left: 8,
+                  top: 8,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          fileImage = null;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.delete,
+                        color: ColorsData.kLightPrimaryColor,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 16),
             ],
           ),
         ),
-      ),
+        SizedBox(height: 12),
+        Container(
+          decoration: ShapeDecoration(
+            color: ColorsData.kContainerFieldColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          padding: EdgeInsets.all(8),
+          child: Text(
+            "Product Image",
+            style: AppStyles.regular16.copyWith(color: const Color(0xFF949D9E)),
+          ),
+        ),
+      ],
     );
+  }
+
+  Future<void> pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    fileImage = File(image!.path);
+    if (fileImage == null) {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 }
