b7d5425340963e6f84d77d75593701e93cd662f7
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-29 22:01:49 +0100

    images repo impl: implement uploadImage

--- lib/core/repositories/images_repo_impl.dart
+++ lib/core/repositories/images_repo_impl.dart
@@ -3,11 +3,25 @@ import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_app_dashboard/core/errors/failures.dart';
 import 'package:ecommerce_app_dashboard/core/repositories/images_repo.dart';
+import 'package:ecommerce_app_dashboard/core/services/storage_service.dart';
+import 'package:ecommerce_app_dashboard/core/utils/backend_break_point.dart';
 
 class ImagesRepoImpl implements ImagesRepo {
+  final StorageService storageService;
+  ImagesRepoImpl(this.storageService);
   @override
-  Future<Either<Failure, String>> uploadImage(File fileImage) {
-    // TODO: implement upload
-    throw UnimplementedError();
+  Future<Either<Failure, String>> uploadImage(
+    File fileImage,
+    String path,
+  ) async {
+    try {
+      String url = await storageService.uploadFile(
+        fileImage,
+        BackendBreakPoint.images,
+      );
+      return Right(url);
+    } on Exception catch (e) {
+      return Left(ServerFailure(message: "Failed to upload image!"));
+    }
   }
 }
