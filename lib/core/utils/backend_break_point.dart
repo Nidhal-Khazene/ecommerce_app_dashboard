b1da6cbb8a510ba58e10d35a259fb41d2fb924a3
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-30 22:13:12 +0100

    backend break point: add productColletions

--- lib/core/utils/backend_break_point.dart
+++ lib/core/utils/backend_break_point.dart
@@ -3,4 +3,5 @@ class BackendBreakPoint {
   static const addUserData = "users";
   static const getUserData = "users";
   static const checkIFUserExists = "users";
+  static const productCollections = "addProducts";
 }
