f1fdcfbb64733a8f5d1e8a92d5c9a80d94e37d5f
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-04 12:03:15 +0100

    add custom loading indicator

---
+++ lib/core/widgets/custom_loading_indicator.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_app_dashboard/core/utils/colors_data.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+
+class CustomLoadingIndicator extends StatelessWidget {
+  const CustomLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoadingIndicator(
+      indicatorType: Indicator.ballPulse,
+
+      /// Required, The loading type of the widget
+      colors: [
+        ColorsData.kPrimaryColor,
+        ColorsData.kSecondaryColor,
+        ColorsData.kLightPrimaryColor,
+      ],
+
+      /// Optional, The color collections
+      strokeWidth: 2,
+
+      /// Optional, The stroke of the line, only applicable to widget which contains line
+      backgroundColor: Colors.transparent,
+
+      /// Optional, Background of the widget
+      pathBackgroundColor: Colors.transparent,
+
+      /// Optional, the stroke backgroundColor
+    );
+  }
+}
