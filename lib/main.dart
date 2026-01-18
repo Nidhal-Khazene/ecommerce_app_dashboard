307a388fcc25df677b449cf0f7975391c5bc69c0
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-18 10:53:48 +0100

    main: add supabase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
-import 'package:ecommerce_app_dashboard/config/api_keys.dart';
 import 'package:ecommerce_app_dashboard/core/routing/on_generate_route.dart';
 import 'package:ecommerce_app_dashboard/core/services/custom_bloc_observer.dart';
 import 'package:ecommerce_app_dashboard/core/services/get_it_service.dart';
+import 'package:ecommerce_app_dashboard/core/services/supabase_storage_service.dart';
 import 'package:ecommerce_app_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
 import 'package:ecommerce_app_dashboard/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: 'https://pfyadbtluwekzzgxnnbi.supabase.co',
-    anonKey: ApiKeys.kSupabaseApiKey,
-  );
+  await SupabaseStorageService.initSupabase();
   Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupGetIt();
